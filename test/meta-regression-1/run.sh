01b739fdedae3ae7845743319b9fde9802777062
Iyad Ba Gari <39617810+bagari@users.noreply.github.com>
2022-05-27 16:30:44 -0700

    Update

--- test/meta-regression-1/run.sh
+++ test/meta-regression-1/run.sh
@@ -17,8 +17,30 @@ APPNAME="/forestplot_$(date +%s)"
 #docker build -t shiny-extras $BASEDIR
 #Should be commented after creating the shiny-extras image.
 
+
+
+effect='EFFECT'
+demographic='MEAN_AGE'
+NonEuro=(GOBS,IMH,UNICAMP,Meth-CT,MIRECC,Meth-CT,MIRECC,UKBB_NonEuropean,OSAKA,PING_NonEuropean,UKBB)
+cohort_size='N'
+sample_size='SAMPLE_SIZE'
+CI_LBB='CI_LB'
+CI_UBB='CI_UB'
+cols=($effect,$sample_size,$demographic,$CI_LBB,$CI_UBB,N,TOTAL_N,PCT)
+Area='Precentral'
+Trait='SA'
+SNP='rs1080066'
+min_val=0
+max_val=120
+demographic_annot='Age'
+
+
 #Running the container. Coping tmp to appname and publishing everithing on $TEMP
-docker run -v $TEMP:$APPNAME --rm shiny-extras /bin/sh -c "cd $APPNAME; Rscript publish.R > publish.log"
+docker run -v $TEMP:$APPNAME --rm shiny-extras /bin/sh -c "cd $APPNAME; R --no-save --slave --args ${effect} ${demographic} ${NonEuro} ${cohort_size} ${cols} ${sample_size} ${CI_LBB} ${CI_UBB} ${Area} ${Trait} ${SNP} ${min_val} ${max_val} ${demographic_annot} < run.R > publish.log"   
+
+
+#Running the container. Coping tmp to appname and publishing everithing on $TEMP
+# docker run -v $TEMP:$APPNAME --rm shiny-extras /bin/sh -c "cd $APPNAME; Rscript publish.R > publish.log"
 
 #creating shiny config
 echo "{\"url\": \"$(tail -1 $TEMP/publish.log | sed 's/^.* //g')\"}" > shinyViz.txt
