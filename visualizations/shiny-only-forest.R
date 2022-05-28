d74577bc2c19197a292205d5ecf5235bbc373f27
Iyad Ba Gari <39617810+bagari@users.noreply.github.com>
2022-05-28 02:33:46 -0700

    Update shiny-only-forest.R

--- visualizations/shiny-only-forest.R
+++ visualizations/shiny-only-forest.R
@@ -12,6 +12,9 @@ library(dplyr)
 
 load("data.RData")
 
+print(Trait)
+print(demographic_annot)
+
 ######### DEFINE HARD CODED PARAMETERS, change these to variables #####################
 pred_length = 100
 cex_lab_value = 0.8 # magnification of x and y labels relative to cex
