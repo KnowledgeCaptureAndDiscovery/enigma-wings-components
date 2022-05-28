7da399f993f889cb6e81db8ac17802d7f5029cb2
Iyad Ba Gari <39617810+bagari@users.noreply.github.com>
2022-05-28 09:21:57 -0700

    Update run.sh

--- test/meta-regression-2/run.sh
+++ test/meta-regression-2/run.sh
@@ -27,15 +27,14 @@ CI_LBB='CI_LB'
 CI_UBB='CI_UB'
 cols=($effect,$sample_size,$demographic,$CI_LBB,$CI_UBB,N,TOTAL_N,PCT)
 Area='Precentral'
-Trait=('Surface Area')
+Trait=(Surface,Area)
 SNP='rs1080066'
 min_val=0
 max_val=120
-demographic_annot=('Female Percentage')
+demographic_annot=(Female,Percentage)
 
 #Running the container. Coping tmp to appname and publishing everithing on $TEMP
-docker run -v $TEMP:$APPNAME --rm shiny-extras /bin/sh -c "cd $APPNAME; Rscript publish.R ${effect} ${demographic} ${NonEuro} ${cohort_size} ${cols} ${sample_size} ${CI_LBB} ${CI_UBB} ${Area} "${Trait[@]}" ${SNP} ${min_val} ${max_val} "${demographic_annot[@]}" > publish.log"
-# docker run -v $TEMP:$APPNAME --rm shiny-extras /bin/sh -c "cd $APPNAME; R < publish.R --no-save --slave --args ${effect} ${demographic} ${NonEuro} ${cohort_size} ${cols} ${sample_size} ${CI_LBB} ${CI_UBB} ${Area} "${Trait[@]}" ${SNP} ${min_val} ${max_val} "${demographic_annot[@]}" > publish.log"   
+docker run -v $TEMP:$APPNAME --rm shiny-extras /bin/sh -c "cd $APPNAME; Rscript publish.R ${effect} ${demographic} ${NonEuro} ${cohort_size} ${cols} ${sample_size} ${CI_LBB} ${CI_UBB} ${Area} ${Trait} ${SNP} ${min_val} ${max_val} ${demographic_annot} > publish.log"   
 
 #creating shiny config
 echo "{\"url\": \"$(tail -1 $TEMP/publish.log | sed 's/^.* //g')\"}" > shinyViz.txt
